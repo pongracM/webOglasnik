43d8d33f6c94690a968d29ad1d0eedbc94597ddf
Marko Pongrac <marko.pongrac@mev.hr>
2023-06-02 22:20:00 +0200

    prijava

--- webOglasnik/Controllers/KorisniciController.cs
+++ webOglasnik/Controllers/KorisniciController.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using System.Web.Security;
+using webOglasnik.Misc;
 using webOglasnik.Models;
 
 namespace webOglasnik.Controllers
 {
     public class KorisniciController : Controller
     {
-        private BazaDbContext db = new BazaDbContext();
-
+        BazaDbContext db = new BazaDbContext();
         // GET: Korisnici
         public ActionResult Index()
         {
-            var listaKorisnika = db.PopisKorisnika.OrderBy(x=>x.SifraOvlasti).ThenBy(x=>x.Prezime).ToList();
-
+            var listaKorisnika = db.PopisKorisnika
+                .OrderBy(x => x.SifraOvlasti).ThenBy(x => x.Prezime).ToList();
             return View(listaKorisnika);
         }
-
         [HttpGet]
         [AllowAnonymous]
         public ActionResult Prijava(string returnUrl)
@@ -26,6 +28,54 @@ namespace webOglasnik.Controllers
             KorisnikPrijava model = new KorisnikPrijava();
             ViewBag.ReturnUrl = returnUrl;
             return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Prijava(KorisnikPrijava model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var korisnikBaza = db.PopisKorisnika.
+                    FirstOrDefault(x => x.KorisnickoIme == model.KorisnickoIme);
+                if (korisnikBaza != null)
+                {
+                    var passwordOK = korisnikBaza.Lozinka == PasswordHelper.IzracunajHash(model.Lozinka);
+                    if (passwordOK)
+                    {
+                        LogiraniKorisnik prijavljeniKorisnik = new LogiraniKorisnik(korisnikBaza);
+                        LogiraniKorisnikSerializeModel serializeModel = new LogiraniKorisnikSerializeModel();
+                        serializeModel.CopyFromUser(prijavljeniKorisnik);
+                        JavaScriptSerializer serializer = new JavaScriptSerializer();
+                        string korisnickiPodaci = serializer.Serialize(serializeModel);
+                        FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket(
+                            1,
+                            prijavljeniKorisnik.Identity.Name,
+                            DateTime.Now,
+                            DateTime.Now.AddDays(1),
+                            false,
+                            korisnickiPodaci);
+                        string ticketEncrypted = FormsAuthentication.Encrypt(authTicket);
+
+                        HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, ticketEncrypted);
+                        Response.Cookies.Add(cookie);
+                        if (!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                        {
+                            return Redirect(returnUrl);
+                        }
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Neispravno korisničko ime ili lozinka");
+            return View(model);
+        }
+
+        public ActionResult Odjava()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
         }
 
     }
--- webOglasnik/Global.asax.cs
+++ webOglasnik/Global.asax.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Script.Serialization;
+using System.Web.Security;
+using webOglasnik.Misc;
 
 namespace webOglasnik
 {
@@ -16,6 +20,23 @@ namespace webOglasnik
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                LogiraniKorisnikSerializeModel serializeModel = serializer.Deserialize<LogiraniKorisnikSerializeModel>(authTicket.UserData);
+
+                LogiraniKorisnik korisnik = new LogiraniKorisnik(authTicket.Name);
+                korisnik.PrezimeIme = serializeModel.PrezimeIme;
+                korisnik.Ovlast = serializeModel.Ovlast;
+
+                HttpContext.Current.User = korisnik;
+            }
         }
     }
 }
