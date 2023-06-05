b152a96002f939542b09c285606010479d4158c9
Marko Pongrac <marko.pongrac@mev.hr>
2023-06-05 16:13:12 +0200

    dodano ovlastiKorisnik

---
+++ webOglasnik/Misc/OvlastiKorisnik.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webOglasnik.Misc
+{
+    public class OvlastiKorisnik
+    {
+        public const string Administrator = "AD";
+        public const string Korisnik = "KO";
+    }
+}
